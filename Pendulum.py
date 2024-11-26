6b5570f48d7721d44632269fd54bc84500a2e145
j90137ze <zakaria.dandan@student.manchester.ac.uk>
2024-11-26 00:32:14 +0000

    Fixed some errors with the calculations

--- Pendulum.py
+++ Pendulum.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import math
 
 #constants
-g = 9.81
+g = -9.81
 m1 = 1
 m2 = 1
 L1 = 100
@@ -22,14 +22,7 @@ o2 = 5
 
 dt = 0.001 
 
-#define 4 differential equations to describe the system. x' = dx/dt
-def fun1(w1):
-    #w1 = th1'
-    return w1
-
-def fun2(w2):
-    #w2 = th2'
-    return w2
+#define 2 differential equations to describe the system. x' = dx/dt
 
 def fun3(th1, th2, w2):
     #o1 = w1'
@@ -47,34 +40,32 @@ def fun4(th1, th2, w1, w2):
     r /= L2*(2*m1 + m2 - m2*math.cos(2*th1 - 2*th2))
     return r
 
-
+def normalize(x):
+    #normalizes a value between -2pi and 2pi
+    if x < 0:
+        return -x%(2*math.pi)
+    else:
+        return x%(2*math.pi)
+    
 def RK(th1, th2, w1, w2, o1, o2, n):
     #returns a single iteration of the Runge-Kutta Algorithm
     if n == 1:
-        k1 = w1
-        k2 = w1+dt*k1/2
-        k3 = w1+dt*k2/2
-        k4 = w1+dt*k3
-        return w1 + dt/6*(k1+2*k2+2*k3+k4)
+        r = w1 + dt*o1
     elif n == 2:
-        k1 = (w2)
-        k2 = (w2+dt*k1/2)
-        k3 = (w2+dt*k2/2)
-        k4 = (w2+dt*k3)
-        return w2 + dt/6*(k1+2*k2+2*k3+k4) 
+        r =  w2 + dt*o2
     elif n == 3:
         k1 = fun3(th1, th2, w2)
         k2 = fun3(th1+dt*k1/2, th2+dt*k1/2 , w2+dt*k1/2)
         k3 = fun3(th1+dt*k2/2, th2+dt*k2/2, w2+dt*k2/2)
         k4 = fun3(th1+dt*k3, th2+dt*k3, w2+dt*k3)
-        return o1 + dt/6*(k1+2*k2+2*k3+k4)
+        r =  o1 + dt/6*(k1+2*k2+2*k3+k4)
     elif n == 4:
         k1 = fun4(th1, th2, w1,  w2)
         k2 = fun4(th1+dt*k1/2, th2+dt*k1/2 ,w1+dt*k1/2 ,w2+dt*k1/2)
         k3 = fun4(th1+dt*k2/2, th2+dt*k2/2, w1+dt*k2/2, w2+dt*k2/2)
         k4 = fun4(th1+dt*k3, th2+dt*k3,w1+dt*k3, w2+dt*k3)
-        return o2 + dt/6*(k1+2*k2+2*k3+k4)
-
+        r =  o2 + dt/6*(k1+2*k2+2*k3+k4)
+    return normalize(r)
 
 
 window = tk.Tk()
@@ -96,6 +87,8 @@ x2 =  x1 + L2*math.sin(th2)
 y2 = y1 + L2*math.cos(th2)
 p2 = canvas.create_oval(x2 - 10, y2 - 10, x2 + 10, y2 + 10,fill = 'white')
 
+canvas.create_oval(screen_width/2 - 5, screen_height/2 - 5, 
+                   screen_width/2 + 5, screen_height/2 + 5,fill = 'red')
 
 def update(th1, th2, w1, w2, o1, o2):
     th1 %= 2*math.pi
@@ -104,14 +97,12 @@ def update(th1, th2, w1, w2, o1, o2):
     th1 = th1 + dt*w1
     th2 = th2 + dt*w2
 
-    w1 = RK(th1, th2, w1, w2, o1, o2, 1)
-    w2 = RK(th1, th2, w1, w2, o1, o2, 2)
+    new_w1 = RK(th1, th2, w1, w2, o1, o2, 1)
+    new_w2 = RK(th1, th2, w1, w2, o1, o2, 2)
+    new_o1 = RK(th1, th2, w1, w2, o1, o2, 3)
+    new_o2 = RK(th1, th2, w1, w2, o1, o2, 4)
 
-    w1 %= 2*math.pi
-    w2 %= 2*math.pi
-
-    o1 = RK(th1, th2, w1, w2, o1, o2, 3)
-    o2 = RK(th1, th2, w1, w2, o1, o2, 4)
+    w1, w2, o1, o2 = new_w1, new_w2, new_o1, new_o2
 
     x1 = screen_width/2 + L1*math.sin(th1)
     y1 = screen_height/2 -L1*math.cos(th1)
@@ -119,11 +110,11 @@ def update(th1, th2, w1, w2, o1, o2):
     x2 =  x1 + L2*math.sin(th2)
     y2 = y1 + L2*math.cos(th2)
 
+    #print(f"{th1}, {th2}, {w1}, {o1}, {o2}")
     canvas.coords(p1, [x1 - 10, y1 - 10, x1 + 10, y1 + 10])
     canvas.coords(p2, [x2 - 10, y2 - 10, x2 + 10, y2 + 10])
 
-
-    canvas.after(10, lambda: update(th1, th2, w1, w2, o1, o2))
+    canvas.after(2, lambda: update(th1, th2, w1, w2, o1, o2))
 
 update(th1, th2, w1, w2, o1, o2)
 window.mainloop()
