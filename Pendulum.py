e8dc168b7602aec2ac188e7c69ecc67b8a210252
j90137ze <zakaria.dandan@student.manchester.ac.uk>
2024-11-27 00:58:46 +0000

    Fixed issues with mechanics logic

--- Pendulum.py
+++ Pendulum.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import math
+import matplotlib.pyplot as plt
 
 #constants
 g = -9.81
@@ -13,17 +14,16 @@ th1 = 100
 th2 = 50
 
 #w1 = th1'. w2 = th2'
-w1 = 5
-w2 = 5
+w1 = math.pi/2
+w2 = math.pi
 
 #o1 = w1'. o2 = w2'
-o1 = 5
-o2 = 5
+o1 = math.pi/5
+o2 = -math.pi/5
 
-dt = 0.001 
+dt = 0.001
 
 #define 2 differential equations to describe the system. x' = dx/dt
-
 def fun3(th1, th2, w2):
     #o1 = w1'
     r = -g*(2*m1 + m2)*math.sin(th1)
@@ -34,18 +34,13 @@ def fun3(th1, th2, w2):
 
 def fun4(th1, th2, w1, w2):
     #o2 = w2'
-    r = 2*math.sin(th1 - th2)*((w1**2)*L1* (m1 + m2))
+    r = (w1**2)*L1*(m1 + m2)
     r += g*(m1 + m2)*math.cos(th1)
     r += (w2**2)*L2*m2*math.cos(th1 - th2)
+    r *= 2*math.sin(th1 - th2)
     r /= L2*(2*m1 + m2 - m2*math.cos(2*th1 - 2*th2))
     return r
 
-def normalize(x):
-    #normalizes a value between -2pi and 2pi
-    if x < 0:
-        return -x%(2*math.pi)
-    else:
-        return x%(2*math.pi)
     
 def RK(th1, th2, w1, w2, o1, o2, n):
     #returns a single iteration of the Runge-Kutta Algorithm
@@ -61,13 +56,19 @@ def RK(th1, th2, w1, w2, o1, o2, n):
         r =  o1 + dt/6*(k1+2*k2+2*k3+k4)
     elif n == 4:
         k1 = fun4(th1, th2, w1,  w2)
-        k2 = fun4(th1+dt*k1/2, th2+dt*k1/2 ,w1+dt*k1/2 ,w2+dt*k1/2)
+        k2 = fun4(th1+dt*k1/2, th2+dt*k1/2, w1+dt*k1/2, w2+dt*k1/2)
         k3 = fun4(th1+dt*k2/2, th2+dt*k2/2, w1+dt*k2/2, w2+dt*k2/2)
         k4 = fun4(th1+dt*k3, th2+dt*k3,w1+dt*k3, w2+dt*k3)
         r =  o2 + dt/6*(k1+2*k2+2*k3+k4)
-    return normalize(r)
+    return r
 
+#used for verficiation
+def energy(th1, th2, w1, w2):
+    KE = 0.5 * m1 * (L1 * w1)**2 + 0.5 * m2 * ((L1 * w1)**2 + (L2 * w2)**2 + 2 * L1 * L2 * w1 * w2 * math.cos(th1 - th2))
+    PE = -m1 * g * L1 * math.cos(th1) - m2 * g * (L1 * math.cos(th1) + L2 * math.cos(th2))
+    return KE + PE
 
+energies = []
 window = tk.Tk()
 canvas = tk.Canvas(window, bg = 'black', highlightthickness= 0)
 
@@ -91,19 +92,30 @@ canvas.create_oval(screen_width/2 - 5, screen_height/2 - 5,
                    screen_width/2 + 5, screen_height/2 + 5,fill = 'red')
 
 def update(th1, th2, w1, w2, o1, o2):
-    th1 %= 2*math.pi
-    th2 %= 2*math.pi
+    
 
-    th1 = th1 + dt*w1
-    th2 = th2 + dt*w2
+    new_th1 = th1 + dt*w1
+    new_th2 = th2 + dt*w2
 
     new_w1 = RK(th1, th2, w1, w2, o1, o2, 1)
     new_w2 = RK(th1, th2, w1, w2, o1, o2, 2)
     new_o1 = RK(th1, th2, w1, w2, o1, o2, 3)
     new_o2 = RK(th1, th2, w1, w2, o1, o2, 4)
 
-    w1, w2, o1, o2 = new_w1, new_w2, new_o1, new_o2
+    th1, th2, w1, w2, o1, o2 = new_th1, new_th2, new_w1, new_w2, new_o1, new_o2
 
+    #using simplectic method instead for updating variables
+
+    """ o1 = (fun3(th1, th2, w2))
+    o2 = (fun4(th1, th2, w1, w2))
+
+    w1 = (w1 + dt*o1)
+    w2 = (w2 + dt*o2)
+
+    th1 += dt * w1
+    th2 += dt * w2"""
+
+    #update canvas
     x1 = screen_width/2 + L1*math.sin(th1)
     y1 = screen_height/2 -L1*math.cos(th1)
 
@@ -111,11 +123,18 @@ def update(th1, th2, w1, w2, o1, o2):
     y2 = y1 + L2*math.cos(th2)
 
     #print(f"{th1}, {th2}, {w1}, {o1}, {o2}")
+    #print("Energy:", energy(th1, th2, w1, w2))
+    energies.append(energy(th1, th2, w1, w2))
+
     canvas.coords(p1, [x1 - 10, y1 - 10, x1 + 10, y1 + 10])
     canvas.coords(p2, [x2 - 10, y2 - 10, x2 + 10, y2 + 10])
 
-    canvas.after(2, lambda: update(th1, th2, w1, w2, o1, o2))
+    canvas.after(int(dt * 1000 + 1), lambda: update(th1, th2, w1, w2, o1, o2))
 
 update(th1, th2, w1, w2, o1, o2)
 window.mainloop()
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot(energies)
+plt.show()
