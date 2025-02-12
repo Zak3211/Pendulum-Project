4bba8ff813e1cbe255712dc65aa19b0b2cd02ef7
zak3211 <zakzakono@gmail.com>
2025-02-12 04:33:09 +0000

    Added display for double pendulum with moving base

--- NEAT Algorithm/DoublePendulum.py
+++ NEAT Algorithm/DoublePendulum.py
@@ -65,4 +65,42 @@ class doublePendulum:
         self.theta1 += self.theta1_*self.dt
         self.theta2 += self.theta1_*self.dt
     
-    #testing if I can push
+def displayPendulum(): 
+    if True: 
+        window = tk.Tk() 
+        canvas = tk.Canvas(window, bg = 'black', highlightthickness= 0) 
+        screen_width = 1000 
+        screen_height = 500 
+        canvas.config(width = screen_width, height = screen_height) 
+        canvas.pack() 
+        by = 150 
+        p = doublePendulum() 
+        x1 = p.xb + p.L1*math.sin(p.theta1) 
+        y1 = by - p.L1*math.cos(p.theta1) 
+        
+        x2 = x1 + p.L2*math.sin(p.theta2) 
+        y2 = y1 - p.L2*math.cos(p.theta2) 
+        
+        line1 = canvas.create_line(p.xb, by, x1, y1, fill = 'white', width = 2) 
+        point1 = canvas.create_oval(x1 - 10, y1 - 10, x1 + 10, y1 + 10, fill='white') 
+        line2 = canvas.create_line(x1, y1, x2, y2, fill = 'white', width = 2) 
+        point2 = canvas.create_oval(x2 - 10, y2 - 10, x2 + 10, y2 + 10, fill='white') 
+        base = canvas.create_line(p.xb-20, by, p.xb+20, by, fill = 'gray', width = 5) 
+        
+    def game_loop(): 
+        p.update() 
+        x1 = p.xb + p.L1*math.sin(p.theta1) 
+        y1 = by - p.L1*math.cos(p.theta1) 
+        x2 = x1 + p.L2*math.sin(p.theta2) 
+        y2 = y1 - p.L2*math.cos(p.theta2) 
+        canvas.coords(line1, [p.xb, by, x1, y1]) 
+        canvas.coords(point1, [x1 - 10, y1 - 10, x1 + 10, y1 + 10]) 
+        canvas.coords(line2, [x1, y1, x2, y2]) 
+        canvas.coords(point2, [x2 - 10, y2 - 10, x2 + 10, y2 + 10]) 
+        canvas.coords(base, [p.xb-20, by, p.xb+20, by]) 
+        canvas.after(2, game_loop) game_loop()
+        
+    game_loop()
+    window.mainloop() 
+
+displayPendulum()
