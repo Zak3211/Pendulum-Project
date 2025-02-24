37a4a3423642bbd97d3ea0067a6af6fbf2cd319d
Zakariar Dandan <zakaria.dandan@student.manchester.ac.uk>
2025-02-24 00:33:40 +0000

    Fixed issue with bias addition

--- NEAT Algorithm/NEAT_net.py
+++ NEAT Algorithm/NEAT_net.py
@@ -24,28 +24,30 @@ class NEATNet:
     def ReLU(self, inputs):
         return np.maximum(0, inputs)
 
-    def add_Node(self):
+    def add_node(self):
         layer = random.randint(1, len(self.layers)-3)
+        self.layers[layer] += 1
 
-        new_weights = np.random.randint(1, 10, self.weights[layer].shape[1])
+        new_weights = np.random.randint(1, 10, self.weights[layer][0].shape)
         self.weights[layer] = np.vstack((self.weights[layer], new_weights))
 
-        new_biases = np.random.randint(1, 10, self.biases[layer].shape[1])
-        self.biases[layer] = np.vstack((self.biases[layer], new_biases))
+        new_biases = np.random.randint(1, 10, 1)/10
+        self.biases[layer] = np.hstack((self.biases[layer], new_biases))
         
         new_weights = np.random.rand(self.weights[layer+1].shape[0], 1)
         self.weights[layer+1] = np.hstack((self.weights[layer+1], new_weights))
     
     def delete_node(self):
         layer = random.randint(1, len(self.layers)-3)
-        while len(self.weights[layer]) == 1:
+        while len(self.weights[layer]) <= 2:
             layer = random.randint(1, len(self.layers)-3)
+        self.layers[layer] -= 1
         node = random.randint(0, len(self.weights[layer]) - 1)
 
         self.weights[layer] = np.delete(self.weights[layer], node, axis = 0)
         self.biases[layer] = np.delete(self.biases[layer], node)
         self.weights[layer+1] = np.delete(self.weights[layer+1], node, axis = 1)
-
+    
     def forward(self, inputs):
         inputs = np.array(inputs)
         for i in range(len(self.weights)):
@@ -54,10 +56,13 @@ class NEATNet:
             inputs = self.ReLU(inputs)
         return inputs[0]
 
-for i in range(1):
+for i in range(100):
     net = NEATNet()
     print([net.weights[i].shape for i in range(len(net.weights))])
     print(net.forward([-3, 1,2,1]))
     net.delete_node()
+    print([net.weights[i].shape for i in range(len(net.weights))])
+    print(net.forward([-3, 1,2,1]))
+    net.add_node()
     print([net.weights[i].shape for i in range(len(net.weights))])
     print(net.forward([-3, 1,2,1]))
